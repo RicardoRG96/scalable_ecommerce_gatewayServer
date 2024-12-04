fdf2115916b830b2e591f803fb18ddb7488f739c
RicardoRG96 <ricardoretamal10@gmail.com>
2024-12-04 11:30:03 -0300

    add jwt authentication filters

--- src/main/java/com/ricardo/scalable/ecommerce/platform/gatewayServer/security/SecurityConfig.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/gatewayServer/security/SecurityConfig.java
@@ -12,9 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;      
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,60 +22,43 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authz -> {
             authz
-                    .requestMatchers(HttpMethod.GET, "/authorized", "/logout")
-                    .permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/users")
-                    .permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/users/register")
-                    .permitAll()
-                    .requestMatchers(
-                            HttpMethod.PUT,
-                            "/api/users/{id}",
-                            "/api/users/change-password/{id}"
-                    )
-                    .hasAnyRole("ADMIN", "USER", "SELLER")
-                    .requestMatchers(
-                            HttpMethod.GET,
-                            "/api/users/{id}",
-                            "/api/users/username/{username}",
-                            "/api/users/email/{email}",
-                            "/api/users"
-                    )
-                    .hasRole("ADMIN")
-                    .requestMatchers(
-                            HttpMethod.PUT,
-                            "/api/users/roles/{id}",
-                            "/api/users/block/{id}",
-                            "/api/users/unlock/{id}"
-                    )
-                    .hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.DELETE, "/api/users/{id}")
-                    .hasRole("ADMIN")
-                    .anyRequest()
-                    .authenticated();
-        }).csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2Login(login -> login.loginPage("/oauth2/authorization/client-app"))
-                .oauth2Client(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
-                        jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, AbstractAuthenticationToken>() {
-
-                                @Override
-                                public AbstractAuthenticationToken convert(Jwt source) {
-                                        Collection<String> roles = source.getClaimAsStringList("roles");
-
-                                        Collection<GrantedAuthority> authorities = roles
-                                                .stream()
-                                                .map(SimpleGrantedAuthority::new)
-                                                .collect(Collectors.toList());
-                                        
-                                        return new JwtAuthenticationToken(source, authorities);
-                                }
-
-                        })
-                ))
-                .build();
+                .requestMatchers(HttpMethod.GET, "/authorized", "/logout")
+                .permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users")
+                .permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users/register")
+                .permitAll()
+                .requestMatchers(
+                        HttpMethod.PUT,
+                        "/api/users/{id}",
+                        "/api/users/change-password/{id}"
+                )
+                .hasAnyRole("ADMIN", "USER", "SELLER")
+                .requestMatchers(
+                        HttpMethod.GET,
+                        "/api/users/{id}",
+                        "/api/users/username/{username}",
+                        "/api/users/email/{email}",
+                        "/api/users"
+                )
+                .hasRole("ADMIN")
+                .requestMatchers(
+                        HttpMethod.PUT,
+                        "/api/users/roles/{id}",
+                        "/api/users/block/{id}",
+                        "/api/users/unlock/{id}"
+                )
+                .hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/users/{id}")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+        })
+        // .addFilter(new JwtAuthent)
+        .csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
     }
 
 }
---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/gatewayServer/security/SimpleGrantedAuthorityJsonCreator.java
@@ -0,0 +1,5 @@
+package com.ricardo.scalable.ecommerce.platform.gatewayServer.security;
+
+public class SimpleGrantedAuthorityJsonCreator {
+
+}
